45dbfb36678ce8a76c4fbb1d41c676244fcaca29
ChatelAnt <74916420+Chatelant@users.noreply.github.com>
2022-11-07 09:24:19 +0100

    Initial commit, simple API

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () =>console.log("Serveur à l'écoute"))
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
